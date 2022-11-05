60653a8a89d0208fc43d14717150b2d167ed1849
Christian Avalos <christianavalos311997@gmail.com>
2022-11-04 23:06:55 -0600

    :tada: creation of the project and added the first dependencies

---
+++ src/test/java/com/pack/shopping/cart/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pack.shopping.cart.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
