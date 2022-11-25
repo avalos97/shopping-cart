c5f518e86a2e6e1f6ac1bf6df0d4c74fb748d36a
Christian Avalos <christianavalos311997@gmail.com>
2022-11-25 10:22:23 -0600

    modify EXPIRATION_TIME to 15 min

--- src/main/java/com/pack/shopping/cart/api/security/Constants.java
+++ src/main/java/com/pack/shopping/cart/api/security/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
     public static final String AUTHORIZATION = "Authorization";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String SECRET_KEY = "SECRET_KEY";
-    public static final long EXPIRATION_TIME = 900_0000; // 15 mins
+    public static final long EXPIRATION_TIME = 900_000; // 15 min
     public static final String ROLE_CLAIM = "roles";
     public static final String AUTHORITY_PREFIX = "ROLE_";
   }
